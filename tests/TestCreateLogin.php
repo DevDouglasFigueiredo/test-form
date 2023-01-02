bfb79894180dc86d83762de26434fd6365df5f2e
Douglas Figueiredo <devdouglasfigueiredo@gmail.com>
2023-01-02 11:24:06 -0300

    adicionado outro metodo de assert

--- tests/TestCreateLogin.php
+++ tests/TestCreateLogin.php
@@ -8,10 +8,12 @@ use tests\PageObject\PageCreateLogin;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 
-class TestCreateLogin extends TestCase{
-    
+class TestCreateLogin extends TestCase
+{
+
     private static WebDriver $driver;
 
     public static function setUpBeforeClass(): void
@@ -19,18 +21,17 @@ class TestCreateLogin extends TestCase{
         $host = 'http://localhost:4444/wd/hub';
         $capabilities = DesiredCapabilities::chrome();
         // self::$driver = RemoteWebDriver::create($host, $capabilities);
-        $options = new ChromeOptions(); 
-        $options->addArguments( [ 'headless' ] ); // método para o navegador nao precisar abrir
-        $capabilities->setCapability( ChromeOptions::CAPABILITY, $options ); 
+        $options = new ChromeOptions();
+        $options->addArguments(['headless']); // método para o navegador nao precisar abrir
+        $capabilities->setCapability(ChromeOptions::CAPABILITY, $options);
         self::$driver = RemoteWebDriver::create($host, $capabilities);
     }
 
-    protected function setUp(): void    
+    protected function setUp(): void
     {
         self::$driver->get("https://www.techlistic.com/p/selenium-practice-form.html");
-        
     }
-    
+
     public function testCreateLoginSucess()
     {
         $this->PageCreateLogin = new PageCreateLogin(self::$driver);
@@ -45,16 +46,20 @@ class TestCreateLogin extends TestCase{
         $this->PageCreateLogin->chooseContinent("South America");
         $this->PageCreateLogin->chooseSeleniumComands();
         $this->PageCreateLogin->clickToDownload();
-        
+
         $this->assertSame(
             "https://github.com/stanfy/behave-rest/blob/master/features/conf.yaml",
             self::$driver->getCurrentURL()
         );
 
+        $this->assertStringContainsString(
+            '© 2023 GitHub, Inc.',
+            self::$driver->getPageSource()
+        );
     }
+
     public function tearDown(): void
     {
         self::$driver->close();
     }
-
 }
